6d5fe7398763c264a1a8f07d861a21647f900728
binumarcodes <abdulrahmanbinumar179@gmail.com>
2025-05-10 13:45:51 +0100

    Change the eslint rules to deploy it on vercel

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  // Add a rules override block
+  {
+    rules: {
+      "react/no-unescaped-entities": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
